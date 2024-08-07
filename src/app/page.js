a1e8fd386fe7e6dd0ea7e9d39407a9f6691c12d1
LUCAS NICOLAS KOZIUPA <47572827@ort>
2024-08-07 13:58:00 -0300

    juego

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import { Boton } from "./components/Boton/page";
 
 export default function Home() {
   const urlApi = "https://countriesnow.space/api/v0.1/countries/flag/images";
   const [paises, setPaises] = useState([]);
   const [randomIndex, setRandomIndex] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [puntos, setPuntos] = useState(0);
+  const [mensaje, setMensaje] = useState('');
 
   useEffect(()=>{
     const fetchPaises = async () => {
@@ -25,9 +29,45 @@ export default function Home() {
     fetchPaises();
   }, [])
 
+  const paisRandom = randomIndex !== null ? paises[randomIndex] : null;
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const verificarRespuesta = () => {
+    if (paisRandom && inputValue.toLowerCase() === paisRandom.name.toLowerCase()) {
+      setPuntos(puntos + 10);
+      setMensaje('¡Correcto!');
+    } else {
+      setPuntos(puntos - 1);
+      setMensaje('Incorrecto. Intenta de nuevo.');
+    }
+    setInputValue('');
+    setRandomIndex(Math.floor(Math.random() * paises.length));
+  };
+
   return (
     <>
-
+      <div>
+        <h1>Adivina la bandera!</h1>
+        {paisRandom && (
+          <>
+            <Image src={paisRandom.flag} alt={paisRandom.name} width={200} height={100}/>
+            <div>
+            <input 
+              type="text" 
+              value={inputValue} 
+              onChange={handleInputChange} 
+              placeholder="Introduce el nombre del país" 
+            />
+            <Boton onClick={verificarRespuesta} label="Verificar"/>
+            </div>
+          </>
+        )}
+        <h2>Puntos: {puntos}</h2>
+        {mensaje && <p>{mensaje}</p>}
+      </div>
     </>
   );
 }
